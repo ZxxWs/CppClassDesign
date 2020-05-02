69fa4942662e025b4c3f412c63f4224dd10519b6
ZxxWs <zxx748361145@outlook.com>
2020-05-02 17:21:52 +0800

    1

--- Header/Search.h
+++ Header/Search.h
@@ -10,6 +10,17 @@ class Search : public QMainWindow
 public:
 	Search(QWidget* parent = Q_NULLPTR);
 
+	QComboBox* GradeList = new QComboBox;
+	QComboBox* NumList = new QComboBox;
+	QLabel* LabNum = new QLabel;
+	QLabel* LabName = new QLabel;
+	QLabel* LabGrade = new QLabel;
+	QLabel* LabScore = new QLabel;
+	QLabel* LabRemake = new QLabel;
+	QTableView* ShowTable=new QTableView;
+	QPushButton* AlterButton=new QPushButton;
+	QPushButton* OutButton=new QPushButton;
+
 private:
 	Ui::Form ui;
 };
--- Source/Search.cpp
+++ Source/Search.cpp
@@ -6,6 +6,17 @@ Search::Search(QWidget* parent)
 {
 	ui.setupUi(this);
 	setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint);//将Search窗口放到Tz窗口中（绑定）
+
+	this->GradeList = ui.GradeList;
+	this->NumList = ui.NumList;
+	this->LabNum = ui.LabNum;
+	this->LabName = ui.LabName;
+	this->LabGrade = ui.LabGrade;
+	this->LabScore = ui.LabScore;
+	this->LabRemake = ui.LabRemark;
+	this->ShowTable=ui.ShowTable;
+	this->AlterButton = ui.AlterButton;
+	this->OutButton = ui.OutButton;
 }
 
 
