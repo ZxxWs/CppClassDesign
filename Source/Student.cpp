53208ae217b2ab5b70b208d0e4a782e5875631ef
ZxxWs <zxx748361145@outlook.com>
2020-05-02 14:07:48 +0800

    1

--- Source/Student.cpp
+++ Source/Student.cpp
@@ -1,15 +1,34 @@
 #include "../Header/Student.h"
-
 #include <iostream>
 using namespace std;
 
-Student::Student(int Snum, int Sgrade, string Sname, double Sscore, string Sdetail, string Sremark){}
-void Student::setSnum(int Snum){}
-void Student::setSgrade(int Sgrade){}
-void Student::setSname(string Sname){}
-void Student::setSscore(double Sscore){}
-void Student::setSdetail(string Sdetail){}
-void Student::setSremark(string Sremark){}
+Student::Student(int Snum, int Sgrade, string Sname, double Sscore, string Sdetail, string Sremark){
+	this->Snum = Snum;
+	this->Sgrade = Sgrade;
+	this->Sname = Sname;
+	this->Sscore = Sscore;
+	setSdetail(Sdetail);
+	this->Sremark = Sremark;
+}
+void Student::setSnum(int Snum){
+	this->Snum = Snum;
+}
+void Student::setSgrade(int Sgrade){
+	this->Sgrade = Sgrade;
+}
+void Student::setSname(string Sname){
+	this->Sname = Sname;
+}
+void Student::setSscore(double Sscore){
+	this->Sscore = Sscore;
+}
+void Student::setSdetail(string Sdetail){
+
+}
+
+void Student::setSremark(string Sremark){
+	this->Sremark = Sremark;
+}
 
 
 int Student::getSnum(){
