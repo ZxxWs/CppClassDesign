bb935947cb249a43f03418d502fe20bea02b5fde
ZxxWs <zxx748361145@outlook.com>
2020-05-05 13:37:10 +0800

    缝缝补补

--- main.cpp
+++ main.cpp
@@ -1,25 +1,10 @@
 #include "Header/Tz.h"
-#include "Header/AddInformation.h"
-#include "Header/Search.h"
-#include "Header/DelInformation.h"
 #include <QtWidgets/QApplication>
-#include <qtextcodec.h>
 
 int main(int argc, char *argv[])
 {
-
-
 	QApplication a(argc, argv);
-	//Tz w;
-	//w.show();
-
-	Search s;
-	s.show();
-
-	//AddInformation add;
-	//add.show();
-
-	//DelInformation dd;
-	//dd.show();
+	Tz w;
+	w.show();
 	return a.exec();
 }
