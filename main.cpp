e19d736f95a1acda79063c5481e4e2abf1514c8c
ZxxWs <zxx748361145@outlook.com>
2020-05-06 00:11:50 +0800

    这次真的不改了，坐等打脸！

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include "Header/Tz.h"
-#include "Header/Search.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);//程序自动生成
-	/*Tz w;
-	w.show();*/
-	Search s;
-	s.show();
+	Tz w;
+	w.show();
+
 	return a.exec();
 }
